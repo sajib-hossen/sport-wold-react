22536c3917ec5c9430084954683c79f2a8defe98
Sajib-Hossene <sajibhossensa90@gmail.com>
2021-10-10 19:23:36 +0600

    update

---
+++ src/components/Simple/Simple.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Simple = () => {
+    return (
+        <div>
+            <h1>this is simple page new page </h1>
+        </div>
+    );
+};
+
+export default Simple;
